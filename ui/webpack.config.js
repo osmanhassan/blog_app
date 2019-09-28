f75c803f0b2205eead80ffc23726b2c257625646
Osman <hassan.osman@divine-it.net>
2019-09-28 11:07:05 +0600

    port changed

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
         new webpack.DefinePlugin({
             // global app config object
             config: JSON.stringify({
-                apiUrl: 'http://localhost:8080'
+                apiUrl: 'http://localhost:8090'
             })
         })
     ],
